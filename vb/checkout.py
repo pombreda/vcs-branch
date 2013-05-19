b5e7b41e7502167216311c4a4a15e591a79e95fe
Takafumi Arakaki <aka.tkf@gmail.com>
2013-05-19 10:31:23 +0200

    Use appropriate remote branch

--- vb/checkout.py
+++ vb/checkout.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import collections
 
 
 def parse_git_remote_v(output):
@@ -23,6 +24,60 @@ def parse_git_remote_v(output):
         yield (remote, url, fop.strip('()'))
 
 
+class GitBranches(object):
+
+    _Branch = collections.namedtuple(
+        'Branch', ['mark', 'branch', 'rbranch', 'remote'])
+
+    def current(self):
+        for item in self.parsed:
+            if item == '*':
+                return item
+
+    def remote(self, branch):
+        for item in self.parsed:
+            if item.branch == branch:
+                return item
+
+    def get(self):
+        output = subprocess.check_output(['git', 'branch', '-vv'])
+        self.parsed = list(self._Branch(*i) for i in self.parse(output))
+
+    @staticmethod
+    def parse(output):
+        r"""
+        Parse output of ``git branch -vv``.
+
+        >>> list(GitBranches.parse('''\
+        ... * B1  d7cab8a [r1/b1] commit message
+        ...   B2  17b657c [r2/b2] commit message
+        ...   B3  203ff99 [r3/b3] commit message
+        ... '''))                           #doctest: +NORMALIZE_WHITESPACE
+        [('*', 'B1', 'r1/b1', 'r1'),
+         ('',  'B2', 'r2/b2', 'r2'),
+         ('',  'B3', 'r3/b3', 'r3')]
+
+        """
+        for line in output.splitlines():
+            if line.startswith('*'):
+                line = line.lstrip('*')
+                mark = '*'
+            else:
+                mark = ''
+            line = line.lstrip()
+            str.split
+            words = line.split(None, 4)
+            (branch, sha) = words[:2]
+            rbranch = remote = ''
+            if len(words) > 2:
+                w2 = words[2]
+                if w2[0] == '[' and w2[-1] == ']':
+                    rbranch = w2[1:-1]
+                    if '/' in rbranch:
+                        remote = rbranch.split('/', 1)[0]
+            yield (mark, branch, rbranch, remote)
+
+
 class CheckoutTask(object):
 
     def __init__(self, branch, locmain='locmain'):
@@ -34,25 +89,49 @@ class CheckoutTask(object):
         if not os.path.isdir('.vb'):
             raise RuntimeError('Not initialized')
 
+    @staticmethod
+    def get_remotes():
+        output = subprocess.check_output(['git', 'remote', '-v'])
+        return dict((l[0], l[1]) for l in parse_git_remote_v(output))
+
+    @staticmethod
+    def get_branches():
+        branches = GitBranches()
+        branches.get()
+        return branches
+
     def run(self):
         self.check_init()
         if os.path.isdir(self.path):
             print("{0} already exists".format(self.path))
             return
 
-        output = subprocess.check_output(['git', 'remote', '-v'])
-        remotes = dict((l[0], l[1]) for l in parse_git_remote_v(output))
+        remotes = self.get_remotes()
+        branches = self.get_branches()
+        rmitem = branches.remote(self.branch)
 
         self.call_at_main(['git', 'clone', '.', self.path])
         self.call_at_clone(['git', 'checkout', self.branch])
 
         self.call_at_clone(['git', 'remote', 'rename', 'origin', self.locmain])
-        for (remote, url) in remotes.items():
-            self.call_at_clone(['git', 'remote', 'add', '-f', remote, url])
-        # FIXME: use "git branch -vv" to find appropriate remote
-        subprocess.call(['git', 'branch', '--set-upstream',
-                         self.branch, 'origin/{0}'.format(self.branch)],
-                        cwd=self.path)
+
+        rbranch = rmitem.rbranch
+        try:
+            url = remotes.pop(rbranch)
+        except KeyError:
+            pass
+        else:
+            self.call_at_clone(['git', 'remote', 'add', '-f', rbranch, url])
+            subprocess.call(['git', 'branch', '--set-upstream',
+                             self.branch, rmitem.rbranch],
+                            cwd=self.path)
+
+        with open(os.devnull, 'w') as devnull:
+            outfile = devnull
+            for (remote, url) in remotes.items():
+                subprocess.Popen(
+                    ['git', 'remote', 'add', '-f', remote, url],
+                    stdin=devnull, stdout=outfile, stderr=subprocess.PIPE)
 
     def call_at_main(self, *args, **kwds):
         subprocess.check_call(*args, **kwds)
