f828c073122d53442542c9d1c474461cdc2c2b7a
Takafumi Arakaki <aka.tkf@gmail.com>
2013-05-21 03:28:24 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+import vb
+
+setup(
+    name='vb',
+    version=vb.__version__,
+    packages=['vb', 'vb.tasks'],
+    author=vb.__author__,
+    author_email='aka.tkf@gmail.com',
+    url='https://github.com/tkf/vcs-branch',
+    license=vb.__license__,
+    description='VB - VCS branching utility',
+    long_description=vb.__doc__,
+    keywords='VCS, Git, branch, CLI',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        # see: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+)
