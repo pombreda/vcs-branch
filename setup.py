31e286a185acdb8406766c0defd595f690e4bba6
Takafumi Arakaki <aka.tkf@gmail.com>
2013-05-21 09:56:06 +0200

    Run tests with envlist = py26, py27, cram

--- setup.py
+++ setup.py
@@ -17,6 +17,9 @@ setup(
         "Development Status :: 3 - Alpha",
         # see: http://pypi.python.org/pypi?%3Aaction=list_classifiers
     ],
+    install_requires=[
+        'argparse',
+    ],
     entry_points={
         'console_scripts': ['vb = vb.cli:main'],
     },
