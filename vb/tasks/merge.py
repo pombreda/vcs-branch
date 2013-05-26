83f9cf30c4767ba159d44640b1b9ddfc50830ad0
Takafumi Arakaki <aka.tkf@gmail.com>
2013-05-26 04:56:11 +0200

    Implement merge --reset(-to-remote)

--- vb/tasks/merge.py
+++ vb/tasks/merge.py
@@ -11,5 +11,27 @@ class MergeTask(MultiBranchTask):
 
         self.check_call(['git', 'merge'] + branches)
 
+        if self.reset or self.reset_to_remote:
+            self.remerge(branches)
+
+    def remerge(self, branches):
+        current = self.get_branches().current()
+        if self.reset_to_remote:
+            self.reset = current.rbranch
+
+        tmp_branch = self.old_branch_format.format(current.branch)
+        self.check_call(['git', 'branch', tmp_branch])
+
+        self.check_call(['git', 'reset', '--hard', self.reset])
+        self.check_call(['git', 'merge'] + branches)
+        diff = self.check_output(['git', 'diff', tmp_branch])
+        if diff.strip():
+            self.logger.warn(
+                'Re-merging produces different result.\n'
+                'Check: git diff %s', tmp_branch)
+        else:
+            self.logger.info('Remove %s', tmp_branch)
+            self.check_call(['git', 'branch', '-D', tmp_branch])
+
 
 task = MergeTask
